2b23ec019e37a8b0409b5e052b7105b6b8a5b4b1
Paolo Mazzon <pdmazzon@gmail.com>
2025-10-05 01:16:33 -0700

    Put gavins code in a unit test

--- backend/backend-server/src/main.rs
+++ backend/backend-server/src/main.rs
@@ -1,17 +1,10 @@
 mod request;
 mod compile;
 mod run_container;
-use crate::run_container::create_runner;
 
 use hyper::{Server};
 use hyper::service::{make_service_fn, service_fn};
 use std::convert::Infallible;
-use crate::compile::compile_c_file;
-use std::fs;
-use std::path::Path;
-
-
-const TMP_DIR: &str = "/tmp/untrusted";
 
 
 #[tokio::main]
@@ -29,37 +22,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     println!("Listening on http://{}", addr);
 
-
-
-    // Create path for binary if path not created
-    let path = Path::new(TMP_DIR);
-    if !path.exists() {
-        fs::create_dir_all(path)?;
-    }
-
-    // Compile c file
-    let temp_file = format!("{}{}", TMP_DIR, "/temp"); // TODO : make temp file name scalable
-    let compiled = compile_c_file("src/test.c", &temp_file);
-    match compiled {
-        Ok(v) =>  {
-            println!("Success: {}", v.display());
-            println!("{}", v.display());
-        }
-        Err(e) => println!("Error: {}", e), // TODO : Pass error to user if it didn't compile through JSON
-    }
-
-    let mut temp_string = TMP_DIR.to_string();
-    temp_string.push_str("/temp");
-    let bin_dir: &str = &temp_string;
-
-
-    match create_runner(bin_dir) {
-        Ok(_) => println!("Created runner."),
-        Err(e) => println!("Error with runner creation: {}", e),
-    }
-    
     // run the server until Ctrl+C
     server.await?;
 
     Ok(())
+}
+
+#[allow(dead_code, unused_imports)]
+mod tests {
+    use crate::run_container::create_runner;
+    use crate::compile::compile_c_file;
+    use std::fs;
+    use std::path::Path;
+    const TMP_DIR: &str = "/tmp/untrusted";
+
+    #[test]
+    fn gavin_test() -> Result<(), anyhow::Error> {
+        // Create path for binary if path not created
+        let path = Path::new(TMP_DIR);
+        if !path.exists() {
+            fs::create_dir_all(path)?;
+        }
+
+        // Compile c file
+        let temp_file = format!("{}{}", TMP_DIR, "/temp"); // TODO : make temp file name scalable
+        let compiled = compile_c_file("src/test.c", &temp_file);
+        match compiled {
+            Ok(v) =>  {
+                println!("Success: {}", v.display());
+                println!("{}", v.display());
+            }
+            Err(e) => println!("Error: {}", e), // TODO : Pass error to user if it didn't compile through JSON
+        }
+
+        let mut temp_string = TMP_DIR.to_string();
+        temp_string.push_str("/temp");
+        let bin_dir: &str = &temp_string;
+
+
+        match create_runner(bin_dir) {
+            Ok(_) => println!("Created runner."),
+            Err(e) => println!("Error with runner creation: {}", e),
+        }
+        Ok(())
+    }
 }
